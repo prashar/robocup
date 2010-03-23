590489d596fc7719fba2ceb915b5becd2c54fd52
Neeraj Prashar <neeraj.prashar@gmail.com>
2010-03-22 18:46:37 -0700

    Adding stuff to dtect corners on field in hope of localization

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ DEPS=
 %.o: %.cpp $(DEPS)
 			$(CC) -c $< -o $(ODIR)/$@ $(CFLAGS) 
 
-hough: hough.o 
+hough: hough.o
 			$(CC) -o $@ $(ODIR)/$< $(LIBS)
 
 objectDetector: objectDetector.o 
@@ -18,65 +18,23 @@ objectDetector: objectDetector.o
 ch6_ex6_1: ch6_ex6_1.o 
 			$(CC) -o $@ $(ODIR)/$< $(LIBS)
 
+ch8_ex2: ch8_ex2.o 
+			$(CC) -o $@ $(ODIR)/$< $(LIBS)
+
+ch8_ex8_2: ch8_ex8_2.o 
+			$(CC) -o $@ $(ODIR)/$< $(LIBS)
+
+ch8_ex8_3: ch8_ex8_3.o 
+			$(CC) -o $@ $(ODIR)/$< $(LIBS)
+
 ch5_ex5_1: ch5_ex5_1.o 
 			$(CC) -o $@ $(ODIR)/$< $(LIBS)
 
-ch5_ex5_2: ch5_ex5_2.o 
+houghex: houghex.o
 			$(CC) -o $@ $(ODIR)/$< $(LIBS)
 
-pickcolor: pickcolor.o 
+houghex2: houghex2.o
 			$(CC) -o $@ $(ODIR)/$< $(LIBS)
 
-ch2_ex2_3: ch2_ex2_3.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch2_ex2_4: ch2_ex2_4.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch2_ex2_5: ch2_ex2_5.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch2_ex2_6: ch2_ex2_6.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch2_ex2_7: ch2_ex2_7.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_3: ch3_ex3_3.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_4: ch3_ex3_4.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_5: ch3_ex3_5.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_9: ch3_ex3_9.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_11: ch3_ex3_11.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_12: ch3_ex3_12.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_13: ch3_ex3_13.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_14: ch3_ex3_14.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_15: ch3_ex3_15.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_17: ch3_ex3_17.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_19: ch3_ex3_19.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-
-ch3_ex3_20: ch3_ex3_20.o 
-			$(CC) -o $@ $(ODIR)/$< $(LIBS)
-			
 clean: 
 		rm -f *.o $(ODIR)/*.o *~ core /*~
--- hough.cpp
+++ hough.cpp
@@ -14,17 +14,34 @@
 #define k_BALLG 22
 #define k_BALLB 0
 
+#define k_FIELDR 147
+#define k_FIELDG 85
+#define k_FIELDB 77
+
 // For Line Detection .. 
 #define THRESH_1 180
-#define WHITE 255 
-#define CANNY_THRESH_1 166
-#define CANNY_THRESH_2 166*3
+#define WHITE 215
+#define CANNY_THRESH_1 0
+#define CANNY_THRESH_2 255
 #define APERTURE_SIZE 3
-#define MAX_THRESHOLD 80
-#define MIN_LINE_LEN 30
-#define MAX_GAP_BET_LINES 20
+#define MAX_THRESHOLD 70
+#define MIN_LINE_LEN 70
+#define MAX_GAP_BET_LINES 5
 #define THICKNESS 3
 #define TYPE_OF_LINE 8
+#define PIX_DIFF 50
+
+// Camera Centre
+#define CAMERA_X 310
+#define CAMERA_Y 250
+
+int global_match ;
+
+void capImage(char * fname,IplImage * pic){
+  if(!cvSaveImage(fname,pic)){
+    printf("Could not save file \n") ;  
+  }
+}
 
 IplImage * detectBall(IplImage * src){
   int w, h ; 
@@ -58,7 +75,113 @@ IplImage * detectBall(IplImage * src){
 
     }
   }
-  cvRectangle(src,cvPoint(mix,miy),cvPoint(max,may),CV_RGB(0,255,0),2,0,0) ; 
+}
+
+// This function just checks whether or not all pixels are accessible or not.
+// TODO: Directly access bytes instead of cvGet2D().
+void checkImagePixels(IplImage * src){
+	  int height=0,width=0;
+
+	  if( src ){
+	    height = src->height ;
+	    width = src->width ;
+	  }
+
+	 for(int i=0 ; i < width ; i++){
+		  for(int k=0 ; k < height ; k++){
+			  CvScalar pix = cvGet2D(src,k,i);
+			  printf("Pixel [%d][%d] has color B[%f]G[%f]R[%f][%f] \n",k,i,pix.val[0],pix.val[1],pix.val[2],pix.val[3]);
+		  }
+	  }
+}
+
+bool match(CvScalar pixcolor, CvScalar comparecolor){
+      if( ((pixcolor.val[0] - k_tol) <= comparecolor.val[0]) && 
+          ((pixcolor.val[0] + k_tol) >= comparecolor.val[0]) && 
+          ((pixcolor.val[1] - k_tol) <= comparecolor.val[1]) && 
+          ((pixcolor.val[1] + k_tol) >= comparecolor.val[1]) && 
+          ((pixcolor.val[2] - k_tol) <= comparecolor.val[2]) && 
+          ((pixcolor.val[2] + k_tol) >= comparecolor.val[2]) ){
+        return true ;   
+      }
+        return false ; 
+}
+
+IplImage * detectCorners(IplImage * src, CvSeq * lines){
+
+	// Need these for clamping ..
+	int height=0,width=0;
+	if( src ){
+		height = src->height ;
+		width = src->width ;
+	}
+	//printf("Width is %d, Height is %d\n",src->width,src->height);
+
+  CvScalar fieldcolor = CV_RGB(k_FIELDR,k_FIELDG,k_FIELDB) ; 
+  CvScalar whitecolor = CV_RGB(255,255,255) ; 
+  unsigned clamp_left,clamp_right,clamp_top,clamp_bottom ;
+  //printf("Total Number of Lines detected: %d\n",lines->total) ;
+
+  // For each line
+  //	For each of the 2 end points(x and y)
+  //		find the (x-10,y) (x+10,y) (x,y+10) (x,y-10) and check whether they are brown or white
+  //		If they fit the model, we need to draw a circle on the point.
+  for (int i = 0; i < lines->total; i++)
+  {
+    CvPoint* line = (CvPoint*)cvGetSeqElem(lines,i);
+    for (int j=0 ; j < 2 ; j++){
+      //printf("Line %d %d\n",line[j].x,line[j].y) ;
+      //CvScalar pixcolor = cvGet2D(src,line[j].y,line[j].x) ;
+      CvMat * mat = (CvMat*)src ;
+
+      //printf("mat has %d rows, and %d cols\n",(unsigned)(mat->cols),(unsigned)(mat->rows));
+
+      // Clamp all values to not exceed image dimensions.
+      if( (line[j].x - PIX_DIFF) <= 0 )
+        clamp_left = 0 ; 
+      else
+        clamp_left = line[j].x - PIX_DIFF ;
+
+      if( (line[j].x + PIX_DIFF) >= width ){
+    	  //printf("Hitting this \n");
+    	  clamp_right = width-1 ;
+      }
+      else{
+    	  clamp_right = line[j].x + PIX_DIFF ;
+    	  //printf("clamp_right set to %d %d\n",clamp_right, width);
+      }
+
+      if( (line[j].y - PIX_DIFF) <= 0 )
+        clamp_bottom = 0 ;
+      else
+        clamp_bottom = line[j].y - PIX_DIFF ;
+
+      if( (line[j].y + PIX_DIFF) >= height )
+        clamp_top = height-1 ;
+      else
+        clamp_top = line[j].y + PIX_DIFF ;
+
+      //printf("\n ct %d, cb %d, cl %d, cr %d \n",clamp_top,clamp_bottom,clamp_left,clamp_right);
+
+      // What is the color of (pix + 10) pixels out from the centre pixel ?
+      //printf("Matching for color_left ... \n");
+      CvScalar pixcolor_left = cvGet2D(src,line[j].y,clamp_left) ;
+     // printf("Matching for color_right where clamp_right %u, and line[j].y is %u ... \n",clamp_right,line[j].y);
+      CvScalar pixcolor_right = cvGet2D(src,line[j].y,clamp_right) ;
+      //printf("Matching for color_right where clamp_top %u, and line[j].x is %u ... ... \n",clamp_top,line[j].x);
+      CvScalar pixcolor_top = cvGet2D(src,clamp_top,line[j].x) ;
+      //printf("Matching for color_top ... \n");
+      CvScalar pixcolor_bottom = cvGet2D(src,clamp_bottom,line[j].x) ;
+
+      //printf("Matching for color ... \n");
+      if( match(pixcolor_left,whitecolor) && match(pixcolor_right,fieldcolor) 
+          && match(pixcolor_top,whitecolor) && match(pixcolor_bottom,fieldcolor) ){
+        cvCircle(src,line[j],5,cvScalar(0,0,255),1); 
+        printf("Match found @ %d, %d\n",line[j].x,line[j].y);
+        global_match=1 ;
+      }
+    }
+	}
   return src ; 
 }
 
@@ -68,6 +191,7 @@ IplImage * detectLines(IplImage *frame){
   IplImage  *white      = NULL;	
   IplImage  *edges      = NULL;
   IplImage  *color_dst  = NULL;
+  IplImage  *edge_detect  = NULL;
   CvSeq* lines = 0;
   CvMemStorage* storage = cvCreateMemStorage(0);
 
@@ -76,6 +200,7 @@ IplImage * detectLines(IplImage *frame){
   white     = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
   edges     = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
   color_dst = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 3);
+  edge_detect = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 3);
 
     // Convert to grayscale
     cvCvtColor(frame, grey, CV_BGR2GRAY);
@@ -88,28 +213,84 @@ IplImage * detectLines(IplImage *frame){
     cvCanny(white, edges, CANNY_THRESH_1 , CANNY_THRESH_2, APERTURE_SIZE);
     color_dst = frame ; 
     // Find all lines int the binary image edges. 
-    lines = cvHoughLines2(edges, storage, CV_HOUGH_PROBABILISTIC, 1, CV_PI/180, MAX_THRESHOLD, MIN_LINE_LEN, MAX_GAP_BET_LINES );
+    lines = cvHoughLines2(edges, storage, CV_HOUGH_PROBABILISTIC, 9, CV_PI/180, MAX_THRESHOLD, MIN_LINE_LEN, MAX_GAP_BET_LINES );
+    //printf("Total Number of Lines detected: %d\n",lines->total) ;
     for (int i = 0; i < lines->total; i++)
     {
       CvPoint* line = (CvPoint*)cvGetSeqElem(lines,i);
+      //printf("P and Q: %d %d\n",line[0].x,line[0].y);  
       cvLine(color_dst, line[0], line[1], CV_RGB(255, 0, 0), THICKNESS, TYPE_OF_LINE);
-	}
+	  }
+/*
+     float rho_arr[100] ; 
+     float theta_arr[100] ; 
+     float par_lines[100][2] ; 
+     lines = cvHoughLines2( edges, storage, CV_HOUGH_STANDARD, 1, CV_PI/180, 75, 0, 0 );
 
-    /*
-    cvNamedWindow("original", CV_WINDOW_AUTOSIZE);
-    cvNamedWindow("grey", CV_WINDOW_AUTOSIZE);
-    cvNamedWindow("white", CV_WINDOW_AUTOSIZE);
-    cvNamedWindow("edges", CV_WINDOW_AUTOSIZE);
-	cvShowImage("grey", grey);
-	cvShowImage("white", white);
-	cvShowImage("edges",edges);
+        for( int i = 0; i < MIN(lines->total,100); i++ )
+        {
+            float* line = (float*)cvGetSeqElem(lines,i);
+            float rho = line[0];
+            float theta = line[1];
+            CvPoint pt1, pt2;
+            double a = cos(theta), b = sin(theta);
+            double x0 = a*rho, y0 = b*rho;
+            pt1.x = cvRound(x0 + 1000*(-b));
+            pt1.y = cvRound(y0 + 1000*(a));
+            pt2.x = cvRound(x0 - 1000*(-b));
+            pt2.y = cvRound(y0 - 1000*(a));
+            cvLine( color_dst, pt1, pt2, CV_RGB(255,0,0), 3, 8 );
+            rho_arr[i] = rho ; 
+            theta_arr[i] = theta ; 
+        }
+        int i=0,j=0,parlines=0 ; 
+        //printf("Total Lines are %d , MIN():%d\n") ; 
+        for(i = 0; i < MIN(lines->total,100); i++ ){
+          //printf("rho[i] %f, theta[i] %f \n",rho_arr[i], theta_arr[i]) ; 
+          for(j = 0; j < MIN(lines->total,100); j++ ){
+            if( i != j){
+                //printf("------->: %f with %f (%lf)\n",theta_arr[i],theta_arr[j], a) ; 
+              //printf("Comparing %f with %f\n",theta_arr[i],theta_arr[j]) ; 
+              double a = theta_arr[i] - theta_arr[j] ; 
+              if(a < 0) a*=-1 ;
+              if(a < 0.02){
+                par_lines[parlines][0] = rho_arr[i] ; 
+                par_lines[parlines][1] = theta_arr[i] ; 
+                parlines++ ; 
+                par_lines[parlines][0] = rho_arr[j] ; 
+                par_lines[parlines][1] = theta_arr[j] ; 
+                parlines++ ; 
+              }
+            }
+          }
+        }
+
+        printf("Parallel Lines :: \n") ; 
+        for( i = 0; i < parlines ; i++ ){
+                //printf("------->: %f with %f \n",par_lines[i][0],par_lines[i][0]) ; 
+                printf("a) rho[i] %f, theta[i] %f \n",par_lines[parlines][0], par_lines[parlines][1]) ; 
+        }
     */
+    edge_detect = detectCorners(frame,lines);
+
+    cvNamedWindow("edge_detect", CV_WINDOW_AUTOSIZE);
+	  cvShowImage("edge_detect", edge_detect);
+	  if(global_match)
+		  while(1){}
+	  global_match=0 ;
+	    cvNamedWindow("grey", CV_WINDOW_AUTOSIZE);
+	    //capImage("cap2.jpg",white) ;
+	    cvNamedWindow("white", CV_WINDOW_AUTOSIZE);
+	    cvNamedWindow("edges", CV_WINDOW_AUTOSIZE);
+	  cvShowImage("grey", grey);
+	  cvShowImage("white", white);
+	  cvShowImage("edges",edges);
 
     return color_dst ; 
 }
 
 void usage(){
-  printf("./hough -v|-a filename\n") ; 
+  printf("./hough -v|-p filename\n") ; 
 }
 
 
@@ -134,7 +315,7 @@ int main(int argc, char* argv[])
         fname = argv[i+1] ; 
       }
       i++ ; 
-    }else if(!strncmp(argv[i],"-a",2)){
+    }else if(!strncmp(argv[i],"-p",2)){
       vidFlagOn = 0 ; 
       if(argv[i+1] == NULL){
         errorFlagOn = 1 ; 
@@ -160,18 +341,24 @@ int main(int argc, char* argv[])
       return 1;
     }
     pic = cvQueryFrame(video) ; 
+    if(vidFlagOn){
     /* Display fps and calculate delay */
-    printf("Video FPS: %2.2f \n", cvGetCaptureProperty(video, CV_CAP_PROP_FPS));
-    delay = (int) (1000/cvGetCaptureProperty(video, CV_CAP_PROP_FPS));
+      printf("Video FPS: %2.2f \n", cvGetCaptureProperty(video, CV_CAP_PROP_FPS));
+      delay = (int) (1000/cvGetCaptureProperty(video, CV_CAP_PROP_FPS));
+    }
   }
 
   window_name = "VisionSimulator" ;  
   cvNamedWindow(window_name, CV_WINDOW_AUTOSIZE);
+  IplImage * processed = NULL ; 
+  int count = 0 ; 
   while(pic)
   {
-    IplImage * processed = detectLines(pic);
+    processed = detectLines(pic);
     // Detect the ball first. 
     detectBall(processed) ; 
+    // Mark Camera
+    cvCircle(processed,cvPoint(CAMERA_X,CAMERA_Y),40,cvScalar(0,0,255),1); 
 	/* show the loaded image with edges detected*/
     cvShowImage(window_name, processed);
 
@@ -192,9 +379,16 @@ int main(int argc, char* argv[])
       return 1;
     }
 
+    // Capture Frame 10..
+    if(count == 180){
+      capImage("cap3.jpg",pic) ; 
+    }
+    //printf("frame_count is %d\n",count) ;
+
     /* wait delay and check for quit key */
     c = cvWaitKey(33);
     if(c == 27) break;
+    count++ ; 
   }
 }
 
